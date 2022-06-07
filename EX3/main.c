32b30ed50939b19b80d52f684ee1a91504633fd9
avielc11 <aviel.cohen2@msmail.ariel.ac.il>
2022-06-07 16:32:28 +0300

    finish

--- EX3/main.c
+++ EX3/main.c
@@ -1,154 +1,237 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+void handle_sigint(int sig)
+{
+    printf("\nYou typed Control-C!\n");
+}
 
 int main() {
+    /* ignore signal ^C */
+    signal(SIGINT, handle_sigint);
+
     /* initilize */
-    char command[1024];
-    char *token, *argv[10], *outfile;
-    int i, fd, amper = 0, redirOut = 0, retid, status, last_status = 0
-        , redirErr = 0;
-    
-    int length_argv = 0;
+    char *argv[10];
+    char *token, *outfile;
+    char command[1024], last_command[1024];
 
-    char *prompt = (char*)malloc(6);  // free. also check if returns value different from -1.
+    int i, fd,  retid, status;
+    int quit = 1, amper = 0, piping = 0,
+        redir_err = 0, redir_out = 0, last_status = 0;
+
+    char prompt[1024];
     strcpy(prompt, "hello");
-    
-    pid_t main_pid = getpid();
 
-    int quit = 1;
+    /* for pipes */
+    char ***args = (char ***)malloc(10 * sizeof(char **));
+    int fildes[2];
+
     while (quit)
     {
-        redirErr = 0; 
-        redirOut = 0;
+        /* return start seeting */
+        piping = 0;
+        redir_out = 0;
+        redir_err = 0;
+
+        /* save the last command */
+        memcpy(last_command, command, 1024);
+
         printf("%s: ", prompt);
         fgets(command, 1024, stdin);
         command[strlen(command) - 1] = '\0';
-        token = strtok(command," ");
-        if(!strcmp(token, "!!")){
-            printf("execute the last command: ");
-            for(int j = 0; argv[j] != NULL; j++){
-                printf("%s ", argv[j]);
-            }
-            printf("\n");
-        }
-        else{
-            /* parse command line */  
-            // token = strtok(command," ");
-            i = 0;
-            while (token != NULL)
-            {
-                if(i < length_argv) free(argv[i]);
-                argv[i] = (char*)malloc(strlen(token) + 1);
-                strcpy(argv[i], token);
-                token = strtok (NULL, " ");
-                i++;
-            }
 
-            if(i < length_argv) free(argv[i]);
-            argv[i] = NULL;
-            int tempLen = i;
-            i++;
-            while(i < length_argv){
-                free(argv[i]);
-                i++;
-            }
-            length_argv = tempLen;
+        /* parse command line */
+        i = 0;
+        token = strtok (command, " ");
+
+        if(token != NULL && !strcmp(token, "!!")) {
+            memcpy(command, last_command, 1024);
+            token = strtok (command, " ");
         }
-        // printf("5\n");
+        
+        while (token != NULL)
+        {
+            argv[i] = token;
+            token = strtok (NULL, " ");
+            i++;
+            if (! piping && token && ! strcmp(token, "|")) {
+                piping = 1;
+                break;
+            }
+        }
+        argv[i] = NULL;
+
         /* Is command empty */
         if (argv[0] == NULL)
             continue;
-        else if(!strcmp(argv[0], "prompt") && argv[1] != NULL && !strcmp(argv[1], "=")){
-            if(argv[2] != NULL){
-                free(prompt);
-                int new_length = strlen(argv[2]);
-                prompt = (char*)malloc(new_length + 1);  // free. also check if returns value different from -1.
-                strcpy(prompt, argv[2]);
-            } else {
-                printf("wrong number of arguments\n");
-            }
-        }
-        /* finish the program */
-        else if(! strcmp(argv[0], "quit")){
+
+        /* exit */
+        if(! strcmp(argv[0], "quit")){
             quit = 0;
-            continue;   // ?
+            continue;
         }
+
         /* Does command line end with & */ 
-        if (! strcmp(argv[length_argv - 1], "&")) {
+        if (! strcmp(argv[i - 1], "&")) {
             amper = 1;
-            argv[length_argv - 1] = NULL;
+            argv[i - 1] = NULL;
         }
-        // else 
-        //     amper = 0; 
-        if (! strcmp(argv[length_argv - 2], ">")) {
-            redirOut = 1;
-            argv[length_argv - 2] = NULL;
-            outfile = argv[length_argv - 1];
-        }
-        else if (! strcmp(argv[length_argv - 2], "2>")) {               
-            redirErr = 1;
-            argv[length_argv - 2] = NULL;
-            outfile = argv[length_argv - 1];
-        }
+
+        /* change directory */
         if(! strcmp(argv[0], "cd")){
             if(argv[1] != NULL) chdir(argv[1]);
             else chdir("/home/aviel");
             continue;
         }
-        // else {
-        //     redirOut = 0;
-        // }
-        if(! strcmp(argv[0], "echo") && ! strcmp(argv[1], "$?")){
-            printf("%d\n", last_status);
+
+        /* get varibles enviroments */
+        if (! strcmp(argv[0], "read"))
+        {
+            for (int j = 1; j < i; j++)
+            {
+                char temp_str[1000] = "$";
+                strcat(temp_str, argv[j]);
+
+                if(getenv(temp_str) != NULL){
+                    printf("%s ", getenv(temp_str));
+                }
+                else
+                    printf("%s ", argv[j]);
+            }
+            printf("\n");
             continue;
+        }
+
+        if(i > 1){
+            /* print */
+            if(! strcmp(argv[0], "echo")){
+                /* print the last exit code */
+                if(! strcmp(argv[1], "$?")){
+                    printf("%d\n", last_status);
+                }
+
+                /* also print varible's enviroment */
+                else {
+                    for (int j = 1; j < i; j++)
+                    {
+                        char *temp_str = argv[j];
+                        if(temp_str[0] == '$' && getenv(temp_str) != NULL)
+                            printf("%s ", getenv(temp_str));
+                        else
+                            printf("%s ", temp_str);
+                    }
+                    printf("\n");
+                }
+                continue;
+            }
+        }
+
+        if(i > 2){
+            if(! strcmp(argv[1], "=")){
+                /* change the prompt */
+                if(! strcmp(argv[0], "prompt")){
+                    strcpy(prompt, argv[2]);
+                }
+                /* insert varible */
+                else {
+                    setenv(argv[0], argv[2], 1);
+                }
+                continue;
+            }
+
+            /* redirect the stdout or stderr to file */
+            redir_out = ! strcmp(argv[i - 2], ">"); // int out
+            redir_err = ! strcmp(argv[i - 2], "2>"); // int err
+            if (redir_out || redir_err) {
+                argv[i - 2] = NULL;
+                outfile = argv[i - 1];
+            }
+        }
+
+        if (piping)
+        {
+            args[0] = argv;
+            int command_num = 1;
+            int word_num = 0;
+            args[command_num] = (char **)malloc(1024 * sizeof(char *));
+            token = strtok(NULL, " ");
+            while (token != NULL)
+            {
+                if (!strcmp(token, "|"))
+                {
+                    args[command_num][word_num] = NULL;
+                    command_num++;
+                    word_num = 0;
+                    args[command_num] = (char **)malloc(1024 * sizeof(char *));
+                    token = strtok(NULL, " ");
+                }
+                args[command_num][word_num] = token;
+                token = strtok(NULL, " ");
+                word_num++;
+            }
+            args[++command_num] = NULL;
         }
 
         /* for commands not part of the shell command language */ 
         if (fork() == 0) { 
-
-            /////////////////////MINE/////////////////////
-            if (redirErr || redirOut) {
-                printf("err = %d, out = %d\n", redirErr, redirOut);
+            /* redirection of stdout or stderr*/
+            if (redir_err || redir_out) {
                 fd = creat(outfile, 0660); 
-                redirErr? close (STDERR_FILENO) : close(STDOUT_FILENO); 
+                if(redir_out) close(STDOUT_FILENO);
+                else close(STDERR_FILENO); 
                 dup(fd); 
                 close(fd);
-                /* stderr or stdout is now redirected */
-            } 
-            //////////////////////////////////////////////
-            /* redirection of IO ? */
-            // else if (redirOut) {
-            //     fd = creat(outfile, 0660); 
-            //     close (STDOUT_FILENO) ; 
-            //     dup(fd); 
-            //     close(fd); 
-            //     /* stdout is now redirected */
-            // } 
-            execvp(argv[0], argv);
+                /* stderr or stdout | stderr is now redirected */
+            }
+
+            /* deal with many pipes */
+            if (piping)
+            {
+                int pipe_in = 0;
+                while (*args)
+                {
+                    pipe(fildes);
+                    if (fork() == 0)
+                    {
+                        dup2(pipe_in, 0);
+                        if (*(args + 1) != NULL)
+                        {
+                            dup2(fildes[1], 1);
+                        }
+                        close(fildes[0]);
+                        execvp((*args)[0], *args);
+                        exit(0);
+                    }
+                    else
+                    {
+                        wait(NULL);
+                        close(fildes[1]);
+                        pipe_in = fildes[0];
+                        args++;
+                    }
+                }
+                exit(0);
+            }
+
+            /* execute the command */
+            else{
+                execvp(argv[0], argv);
+            }
             quit = 0;
+
         }
+
         /* parent continues here */
-        else if (amper == 0){
-            retid = wait(&status);
-        }
+        
+        if (amper == 0) retid = wait(&status);
+
+        /* save the last exit code */
         last_status = WEXITSTATUS(status);
     }
-    
-    if(getpid() == main_pid){
-        free(prompt);
-        for(i = 0; argv[i] != NULL; i++){
-            free(argv[i]);
-        }
-        printf("good bye\n");
-    }
 }
-
-
-
-
